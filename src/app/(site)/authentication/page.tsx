3afa195f7861688a5ac33e7bf7345c04ddf87bcb
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-04-11 21:03:26 +0100

    google authentication

--- src/app/(site)/authentication/page.tsx
+++ src/app/(site)/authentication/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link"
-import { GoogleAuthProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth"
+import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth"
 import { auth } from "@/database/config";
 import { inputProps } from "@/app/interfaces/interface";
 import { useRouter } from "next/navigation";
@@ -14,6 +14,8 @@ const Authenticate = () => {
     const [authType, setAuthType] = useState<string>("signin");
     const [userData, setUserData] = useState<inputProps>({email: "", password: ""});
     const [errorInfo, setErrorInfo] = useState<string>("");
+
+    const provider = new GoogleAuthProvider();
 
     onAuthStateChanged(auth, (user) => {
         if(user){
@@ -75,6 +77,31 @@ const Authenticate = () => {
             setUserData({email:"", password:"", username: ""})
     }
 
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential?.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user)
+            router.push('/')
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+
+    }
+
     const switchAuthenticationType = () => {
         if(authType === 'signin'){
             setAuthType('signup')
@@ -123,10 +150,10 @@ const Authenticate = () => {
                         <span className="h-[1px] bg-gray-600 w-1/4"></span>
                     </div>
 
-                    <button onClick={() => {}} className=" text-white flex items-center gap-2 font-semibold justify-center bg-green-500 p-4 w-full rounded-md my-4 hover:scale-95 transition-all">
+                    <div onClick={signInWithGoogle} className=" text-white flex items-center gap-2 font-semibold justify-center bg-green-500 p-4 w-full rounded-md my-4 hover:scale-95 transition-all">
                         <svg viewBox="-0.5 0 48 48" className="w-6" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Google-color</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"> <g id="Color-" transform="translate(-401.000000, -860.000000)"> <g id="Google" transform="translate(401.000000, 860.000000)"> <path d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24" id="Fill-1" fill="#FBBC05"> </path> <path d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333" id="Fill-2" fill="#EB4335"> </path> <path d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667" id="Fill-3" fill="#34A853"> </path> <path d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24" id="Fill-4" fill="#4285F4"> </path> </g> </g> </g> </g></svg>
                         Sign in with Google
-                    </button>
+                    </div>
 
                     <p className="text-gray-500 text-sm text-right">{authType === 'signin' ? 'Not a user yet? ': 'Have an existing account? ' } 
                     <span className="text-highlight underline font-bold transition-all text-black cursor-pointer" 
