7521f1289c9178b61745ca2ff043d2d694d91654
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-04-18 12:16:39 +0100

    enable analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import StoreProvider from "../StoreProvider";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           {children}
           <Footer/>
         </StoreProvider>
+        <Analytics/>
         </body>
     </html>
   );
