6a0645c313d1883d5f0eef3eef40c1ac0e4ca966
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-12-23 12:17:39 +0100

    deploy fix

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -21,8 +21,8 @@ const Recommendations = async () => {
         <h2 className="my-4 font-semibold font-lg uppercase styreneBold">Related products</h2>
         <section className="smd:grid-cols-3 lg:grid-cols-4 grid grid-cols-2 gap-4">
             {random && random.map((item:feature) => {
-                const { imageUrl, name, price, _id, slug } = item
-                return <Product key={_id} imageUrl={imageUrl} name={name} price={price} _id={_id} slug={slug} />
+                const { imageUrl, name, price, _id, slug, stock } = item
+                return <Product key={_id} imageUrl={imageUrl} name={name} price={price} _id={_id} slug={slug} stock={stock}/>
             })}
         </section>
     </div>
